63e2da334a1bc1d75e4cd27ecc8e926f313eaa51
luisucan <luis_alberto_ucan@hotmail.com>
2026-05-04 13:28:46 -0500

    fix list printers in mac version 2

--- src/core/usb/EscPosPrinterMacOs.ts
+++ src/core/usb/EscPosPrinterMacOs.ts
@@ -40,7 +40,7 @@ export class EscPosPrinterMacOs extends EscPosPrinterImpl {
 
   async getListPrinters(): Promise<PrinterInfo[]>{
     return new Promise((resolve, reject)=>{
-      execFile('/usr/bin/lpstat', ['-p'], (error, stdout)=>{
+      execFile('/usr/bin/lpstat', ['-p'], { env: { ...process.env, LANG: 'C', LC_ALL: 'C' } }, (error, stdout)=>{
         if(error){
           reject(error)
           return
